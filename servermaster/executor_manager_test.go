34f80139ced5f4a3da3739f3c9aefc90c1237dd9
amyangfei <amyangfei@gmail.com>
2022-01-21 15:54:01 +0800

    add allocate test

--- servermaster/executor_manager_test.go
+++ servermaster/executor_manager_test.go
@@ -20,8 +20,9 @@ func TestExecutorManager(t *testing.T) {
 	mgr := NewExecutorManagerImpl(heartbeatTTL, checkInterval, nil)
 
 	// register an executor server
+	executorAddr := "127.0.0.1:10001"
 	registerReq := &pb.RegisterExecutorRequest{
-		Address:    "127.0.0.1:10001",
+		Address:    executorAddr,
 		Capability: 2,
 	}
 	info, err := mgr.AllocateNewExec(registerReq)
@@ -41,9 +42,27 @@ func TestExecutorManager(t *testing.T) {
 		}
 	}
 
+	// test executor heartbeat
 	resp, err := mgr.HandleHeartbeat(newHeartbeatReq())
 	require.Nil(t, err)
 	require.Nil(t, resp.Err)
+
+	// test allocate resource to given task request
+	tasks := []*pb.ScheduleTask{
+		{
+			Task: &pb.TaskRequest{Id: 1, OpTp: int32(model.JobMasterType)},
+			Cost: 1,
+		},
+	}
+	allocated, allocResp := mgr.Allocate(tasks)
+	require.True(t, allocated)
+	require.Equal(t, 1, len(allocResp.GetSchedule()))
+	require.Equal(t, map[int64]*pb.ScheduleResult{
+		1: {
+			ExecutorId: string(info.ID),
+			Addr:       executorAddr,
+		},
+	}, allocResp.GetSchedule())
 
 	mgr.Start(ctx)
 
@@ -53,6 +72,7 @@ func TestExecutorManager(t *testing.T) {
 	require.Equal(t, 0, len(mgr.executors))
 	mgr.mu.Unlock()
 
+	// test late heartbeat request after executor is offline
 	resp, err = mgr.HandleHeartbeat(newHeartbeatReq())
 	require.Nil(t, err)
 	require.NotNil(t, resp.Err)
