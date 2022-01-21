31bb6979fc48e0bd6677b4d1fdd0732c51b9f626
amyangfei <amyangfei@gmail.com>
2022-01-21 15:22:48 +0800

    executor-manager: refine keepalive config, add unit tests

---
+++ servermaster/executor_manager_test.go
@@ -0,0 +1,60 @@
+package servermaster
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/hanfei1991/microcosm/model"
+	"github.com/hanfei1991/microcosm/pb"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExecutorManager(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	heartbeatTTL := time.Millisecond * 100
+	checkInterval := time.Millisecond * 10
+	mgr := NewExecutorManagerImpl(heartbeatTTL, checkInterval, nil)
+
+	// register an executor server
+	registerReq := &pb.RegisterExecutorRequest{
+		Address:    "127.0.0.1:10001",
+		Capability: 2,
+	}
+	info, err := mgr.AllocateNewExec(registerReq)
+	require.Nil(t, err)
+
+	mgr.mu.Lock()
+	require.Equal(t, 1, len(mgr.executors))
+	require.Contains(t, mgr.executors, info.ID)
+	mgr.mu.Unlock()
+
+	newHeartbeatReq := func() *pb.HeartbeatRequest {
+		return &pb.HeartbeatRequest{
+			ExecutorId: string(info.ID),
+			Status:     int32(model.Running),
+			Timestamp:  uint64(time.Now().Unix()),
+			Ttl:        uint64(10), // 10ms ttl
+		}
+	}
+
+	resp, err := mgr.HandleHeartbeat(newHeartbeatReq())
+	require.Nil(t, err)
+	require.Nil(t, resp.Err)
+
+	mgr.Start(ctx)
+
+	// sleep to wait executor heartbeat timeout
+	time.Sleep(time.Millisecond * 30)
+	mgr.mu.Lock()
+	require.Equal(t, 0, len(mgr.executors))
+	mgr.mu.Unlock()
+
+	resp, err = mgr.HandleHeartbeat(newHeartbeatReq())
+	require.Nil(t, err)
+	require.NotNil(t, resp.Err)
+	require.Equal(t, pb.ErrorCode_UnknownExecutor, resp.Err.GetCode())
+}
