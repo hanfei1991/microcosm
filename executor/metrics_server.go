8d3afc915205155af0ae94844f61a5cfec4a74f1
amyangfei <amyangfei@gmail.com>
2022-02-19 23:51:36 +0800

    fix make check

--- executor/metrics_server.go
+++ executor/metrics_server.go
@@ -4,15 +4,13 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-var (
-	executorTaskNumGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Namespace: "dataflow",
-			Subsystem: "executor",
-			Name:      "task_num",
-			Help:      "number of task in this executor",
-		}, []string{"status"})
-)
+var executorTaskNumGauge = prometheus.NewGaugeVec(
+	prometheus.GaugeOpts{
+		Namespace: "dataflow",
+		Subsystem: "executor",
+		Name:      "task_num",
+		Help:      "number of task in this executor",
+	}, []string{"status"})
 
 // initServerMetrics registers statistics of executor server
 func initServerMetrics(registry *prometheus.Registry) {
