0c97340086d4d1ac45eb27f430634d023c2c9959
Zixiong Liu <liuzixiong@pingcap.com>
2022-03-16 12:00:15 +0800

    pkg/externalresource: implement meta accessor

--- go.mod
+++ go.mod
@@ -29,6 +29,7 @@ require (
 	go.uber.org/atomic v1.9.0
 	go.uber.org/dig v1.13.0
 	go.uber.org/goleak v1.1.12
+	go.uber.org/ratelimit v0.2.0
 	go.uber.org/zap v1.20.0
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba
---
+++ pkg/externalresource/resourcemeta/model.go
@@ -0,0 +1,30 @@
+package resourcemeta
+
+import (
+	"time"
+
+	"github.com/hanfei1991/microcosm/model"
+)
+
+type (
+	WorkerID   = string
+	ResourceID = string
+	JobID      = string
+	ExecutorID = model.ExecutorID
+)
+
+// ResourceMeta is the records stored in the metastore.
+type ResourceMeta struct {
+	ID       ResourceID `json:"id"`
+	Job      JobID      `json:"job"`
+	Worker   WorkerID   `json:"worker"`
+	Executor ExecutorID `json:"executor"`
+	Deleted  bool       `json:"deleted"`
+}
+
+// GCTodoEntry records a future need for GC'ing a resource.
+type GCTodoEntry struct {
+	ID           ResourceID `json:"id"`
+	Job          JobID      `json:"job"`
+	TargetGCTime time.Time  `json:"target_gc_time"`
+}
