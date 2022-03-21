f7afc1fa01371ec2e1517854ac32a4abe9d26d39
Zixiong Liu <liuzixiong@gmail.com>
2022-03-21 13:03:35 +0800

    pkg/dataset: implement a generic dataset

--- pkg/dataset/dataset.go
+++ pkg/dataset/dataset.go
@@ -11,18 +11,20 @@ import (
 	"github.com/hanfei1991/microcosm/pkg/meta/metaclient"
 )
 
-// DataSet
-//nolint:structcheck
+// DataSet is a generic layer for using CRUD patterns with KV-backed storage.
+// nolint:structcheck
 type DataSet[E any, T DataEntry[E]] struct {
 	metaclient metaclient.KV
 	keyPrefix  adapter.KeyAdapter
 }
 
+// DataEntry is a type constraint for individual records.
 type DataEntry[E any] interface {
-	ID() string
+	GetID() string
 	*E
 }
 
+// NewDataSet returns a new DataSet.
 func NewDataSet[E any, T DataEntry[E]](metaclient metaclient.KV, keyPrefix adapter.KeyAdapter) *DataSet[E, T] {
 	return &DataSet[E, T]{
 		metaclient: metaclient,
@@ -30,6 +32,7 @@ func NewDataSet[E any, T DataEntry[E]](metaclient metaclient.KV, keyPrefix adapt
 	}
 }
 
+// Get point-gets a record by ID.
 func (d *DataSet[E, T]) Get(ctx context.Context, id string) (T, error) {
 	getResp, kvErr := d.metaclient.Get(ctx, d.getKey(id))
 	if kvErr != nil {
@@ -48,23 +51,44 @@ func (d *DataSet[E, T]) Get(ctx context.Context, id string) (T, error) {
 	return &retVal, nil
 }
 
+// Upsert updates or inserts a record.
 func (d *DataSet[E, T]) Upsert(ctx context.Context, entry T) error {
 	rawBytes, err := json.Marshal(entry)
 	if err != nil {
 		return errors.Trace(err)
 	}
 
-	if _, err := d.metaclient.Put(ctx, d.getKey(entry.ID()), string(rawBytes)); err != nil {
+	if _, err := d.metaclient.Put(ctx, d.getKey(entry.GetID()), string(rawBytes)); err != nil {
 		return err
 	}
 	return nil
 }
 
+// Delete removes a record.
 func (d *DataSet[E, T]) Delete(ctx context.Context, id string) error {
 	if _, err := d.metaclient.Delete(ctx, d.getKey(id)); err != nil {
 		return err
 	}
 	return nil
+}
+
+// LoadAll loads all records.
+func (d *DataSet[E, T]) LoadAll(ctx context.Context) ([]T, error) {
+	getResp, kvErr := d.metaclient.Get(ctx, d.keyPrefix.Path(), metaclient.WithPrefix())
+	if kvErr != nil {
+		return nil, errors.Trace(kvErr)
+	}
+
+	var ret []T
+	for _, kv := range getResp.Kvs {
+		rawBytes := kv.Value
+		var val E
+		if err := json.Unmarshal(rawBytes, &val); err != nil {
+			return nil, errors.Trace(err)
+		}
+		ret = append(ret, &val)
+	}
+	return ret, nil
 }
 
 func (d *DataSet[E, T]) getKey(id string) string {
--- pkg/dataset/dataset_test.go
+++ pkg/dataset/dataset_test.go
@@ -2,6 +2,7 @@ package dataset
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -15,11 +16,11 @@ type record struct {
 	Value int
 }
 
-func (r *record) ID() string {
+func (r *record) GetID() string {
 	return r.RID
 }
 
-func TestDatasetBasics(t *testing.T) {
+func TestDataSetBasics(t *testing.T) {
 	mockKV := mock.NewMetaMock()
 	dataset := NewDataSet[record, *record](mockKV, adapter.MasterInfoKey)
 	err := dataset.Upsert(context.TODO(), &record{
@@ -47,4 +48,20 @@ func TestDatasetBasics(t *testing.T) {
 	_, err = dataset.Get(context.TODO(), "123")
 	require.Error(t, err)
 	require.Regexp(t, ".*ErrDatasetEntryNotFound.*", err.Error())
+}
+
+func TestDataSetLoadAll(t *testing.T) {
+	mockKV := mock.NewMetaMock()
+	dataset := NewDataSet[record, *record](mockKV, adapter.MasterInfoKey)
+	for i := 0; i < 1024; i++ {
+		err := dataset.Upsert(context.TODO(), &record{
+			RID:   fmt.Sprintf("%d", i),
+			Value: i,
+		})
+		require.NoError(t, err)
+	}
+
+	recs, err := dataset.LoadAll(context.TODO())
+	require.NoError(t, err)
+	require.Len(t, recs, 1024)
 }
