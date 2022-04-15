34fa0b44074c9b30eb039eed414eda2003588069
Zixiong Liu <liuzixiong@pingcap.com>
2022-04-15 22:54:54 +0800

    fix runLeaderService panic

--- servermaster/server.go
+++ servermaster/server.go
@@ -670,9 +670,11 @@ func (s *Server) runLeaderService(ctx context.Context) (err error) {
 	defer func() {
 		s.leader.Store(&Member{})
 		s.resign()
-		err := s.jobManager.Close(ctx)
-		if err != nil {
-			log.L().Warn("job manager close with error", zap.Error(err))
+		if s.jobManager != nil {
+			err := s.jobManager.Close(ctx)
+			if err != nil {
+				log.L().Warn("job manager close with error", zap.Error(err))
+			}
 		}
 	}()
 
