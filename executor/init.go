678dcffdcef3808444c98c32d416d0a4dc8cbc65
gmhdbjd <gmhdbjd@gmail.com>
2022-05-20 16:29:28 +0800

    update package

--- executor/init.go
+++ executor/init.go
@@ -1,8 +1,8 @@
 package executor
 
 import (
-	_ "github.com/hanfei1991/microcosm/dm" // register dm
 	cvstask "github.com/hanfei1991/microcosm/executor/cvsTask"
+	dmtask "github.com/hanfei1991/microcosm/executor/dmTask"
 	cvs "github.com/hanfei1991/microcosm/jobmaster/cvsJob"
 	"github.com/hanfei1991/microcosm/jobmaster/dm"
 	"github.com/hanfei1991/microcosm/lib/registry"
@@ -12,5 +12,6 @@ func init() {
 	cvstask.RegisterWorker()
 	cvs.RegisterWorker()
 	dm.RegisterWorker()
+	dmtask.RegisterWorker()
 	registry.RegisterFake(registry.GlobalWorkerRegistry())
 }
